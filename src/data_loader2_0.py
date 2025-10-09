c015e5a9c573b99cfe0b58236d02329195c068cb
JotaPedroCrds <141438696+JotaPedroCrds@users.noreply.github.com>
2025-10-09 13:42:39 -0300

    Add files via upload

---
+++ src/data_loader2_0.py
@@ -0,0 +1,251 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import Levenshtein as lev
+from scipy.stats import ttest_ind
+import unicodedata
+
+# Dados de Volumetria do Almoço
+
+df_volumetria_almoco = pd.read_csv(r'C:\Users\João Pedro\Pessoal\Estudos\MS571\data\volumetria_almoco.csv')
+
+def try_parsing_datetime(x):
+    fmt = '%Y-%m-%d'
+    try:
+        return pd.to_datetime(x, format=fmt)
+    except (ValueError, TypeError):
+        return pd.NaT
+df_volumetria_almoco['Data'] = df_volumetria_almoco['Data'].apply(try_parsing_datetime)
+
+
+# Dados de Volumetria da Janta
+
+df_volumetria_janta = pd.read_csv(r'C:\Users\João Pedro\Pessoal\Estudos\MS571\data\volumetria_janta.csv')
+
+df_volumetria_janta['Data'] = df_volumetria_janta['Data'].apply(try_parsing_datetime)
+
+
+# Dados do Cardápio
+
+df_cardapio = pd.read_csv(r'C:\Users\João Pedro\Pessoal\Estudos\MS571\data\cardapio.csv')
+
+
+df_cardapio['date'] = df_cardapio['date'].apply(try_parsing_datetime)
+
+df_cardapio_almoco_padrao = df_cardapio.loc[df_cardapio['refeicao'] == "Almoço - Cardápio Padrão"]
+
+df_cardapio_janta_padrao = df_cardapio.loc[df_cardapio['refeicao'] == "Jantar - Cardápio Padrão"]
+
+
+# Dados de Precipitação 2024
+
+df_precipitacao_2024 = pd.read_csv(r'C:\Users\João Pedro\Pessoal\Estudos\MS571\data\precip_horarios_2024_Cepagri.csv')
+
+
+prec_por_refeicao_2024 = df_precipitacao_2024["Precipitação"].groupby(df_precipitacao_2024.index // 12).sum()
+
+df_prec_por_refeicao_2024 = prec_por_refeicao_2024.to_frame()
+
+
+df_prec_por_refeicao_2024["Refeição"] = df_prec_por_refeicao_2024.index.map(lambda x: "Almoço" if x % 2 == 0 else "Jantar")
+
+
+df_prec_almoco_2024 = df_prec_por_refeicao_2024.loc[df_prec_por_refeicao_2024['Refeição'] == "Almoço"]
+
+
+df_prec_janta_2024 = df_prec_por_refeicao_2024.loc[df_prec_por_refeicao_2024['Refeição'] == "Jantar"]
+
+
+# Dados de Precipitação 2025
+
+
+df_precipitacao_2025 = pd.read_csv(r'C:\Users\João Pedro\Pessoal\Estudos\MS571\data\precip_horarios_2025parcial_Cepagri.csv')
+
+
+prec_por_refeicao_2025 = df_precipitacao_2025["Precipitação"].groupby(df_precipitacao_2025.index // 12).sum()
+
+df_prec_por_refeicao_2025 = prec_por_refeicao_2025.to_frame()
+
+
+df_prec_por_refeicao_2025["Refeição"] = df_prec_por_refeicao_2025.index.map(lambda x: "Almoço" if x % 2 == 0 else "Jantar")
+
+
+df_prec_almoco_2025 = df_prec_por_refeicao_2025.loc[df_prec_por_refeicao_2025['Refeição'] == "Almoço"]
+
+df_prec_janta_2025 = df_prec_por_refeicao_2025.loc[df_prec_por_refeicao_2025['Refeição'] == "Jantar"]
+
+
+# Dados Precipitação 2024 + 2025
+
+df_prec_almoco_2024_2025 = pd.concat([df_prec_almoco_2024, df_prec_almoco_2025])
+
+
+df_prec_janta_2024_2025 = pd.concat([df_prec_janta_2024, df_prec_janta_2025])
+
+
+df_prec_janta_2024_2025 = df_prec_janta_2024_2025.reset_index(drop=True)
+
+df_prec_almoco_2024_2025 = df_prec_almoco_2024_2025.reset_index(drop=True)
+
+# Main Table
+
+#Criando a main do almoço e da janta a partir da tabela da volumetria
+
+main_almoco_raw = df_volumetria_almoco
+
+main_janta_raw = df_volumetria_janta
+
+main_almoco_raw["Precipitação"] = df_prec_almoco_2024_2025["Precipitação"].reindex(main_almoco_raw.index)
+
+main_janta_raw["Precipitação"] = df_prec_janta_2024_2025["Precipitação"].reindex(main_janta_raw.index)
+
+main_almoco_raw = main_almoco_raw.set_index("Data")
+
+main_janta_raw = main_janta_raw.set_index("Data")
+
+df_cardapio_almoco_padrao = df_cardapio_almoco_padrao.set_index("date")
+
+df_cardapio_janta_padrao = df_cardapio_janta_padrao.set_index("date")
+
+main_almoco_raw = main_almoco_raw.join(df_cardapio_almoco_padrao["prato"], how="left")
+
+main_janta_raw = main_janta_raw.join(df_cardapio_janta_padrao["prato"], how="left")
+
+main_janta_raw = main_janta_raw.rename(columns={"prato": "Prato Principal"})
+
+
+main_almoco_raw = main_almoco_raw.rename(columns={"prato": "Prato Principal"})
+
+
+# Produto Final
+
+main_almoco_sem_nan = main_almoco_raw.dropna().copy()
+
+main_janta_sem_nan = main_janta_raw.dropna().copy()
+
+
+categorias = {}
+
+def tirar_acento(texto):
+    texto_normalizado = unicodedata.normalize('NFKD', texto)
+    texto_sem_acentos = ''.join(c for c in texto_normalizado if not unicodedata.combining(c))
+    return texto_sem_acentos
+
+def limpar(descricao):
+# tirar tudo depois de parenteses
+    descricao = descricao.split('(')[0]
+    descricao = descricao.lower()
+    descricao = tirar_acento(descricao)
+    descricao_palavras = descricao.split()
+    palavras_reais = []
+    for palavra in descricao_palavras:
+        if len(palavras_reais) == 2:
+            break
+        if len(palavra) > 3:
+            palavras_reais.append(palavra)
+    descricao = ' '.join(palavras_reais)
+    return descricao
+
+def categorizar(descricao):
+    threshold = 0.8
+    descricao_limpa = limpar(descricao)
+    for categoria in categorias.keys():
+        if lev.ratio(descricao_limpa, categoria) > threshold:
+            categorias[categoria].append(descricao)
+            return categoria
+    categorias[descricao_limpa] = [descricao]
+    return descricao_limpa
+
+
+main_almoco_sem_nan["Categoria do Prato"] = main_almoco_sem_nan["Prato Principal"].apply(categorizar)
+main_janta_sem_nan["Categoria do Prato"] = main_janta_sem_nan["Prato Principal"].apply(categorizar)
+
+
+almoco_categorias = main_almoco_sem_nan.groupby("Categoria do Prato")["Total"].mean()
+
+
+almoco_categorias_ordenadas = almoco_categorias.sort_values(ascending=True)
+
+
+janta_categorias = main_janta_sem_nan.groupby("Categoria do Prato")["Total"].mean()
+
+janta_categorias_ordenadas = janta_categorias.sort_values(ascending=True)
+
+
+main_todos = [main_almoco_sem_nan, main_janta_sem_nan]
+main_todos = pd.concat(main_todos)
+main_categorias = main_todos.groupby("Categoria do Prato")["Total"].mean()
+main_categorias_ordenadas = main_categorias.sort_values(ascending=False)
+main_almoco_sem_nan = main_almoco_sem_nan.sample(frac=1)
+main_janta_sem_nan = main_janta_sem_nan.sample(frac=1)
+
+def load_data(restaurante):
+    dados_treino_almoço = main_almoco_sem_nan[0:300]
+    dados_treino_janta = main_janta_sem_nan[0:300]
+    dados_test_almoço = main_almoco_sem_nan[301:351]
+    dados_test_janta = main_janta_sem_nan[301:351]
+    training_data_almoco = []
+    training_data_janta = []
+    test_data_almoco = []
+    test_data_janta = []
+    result_training_almoco = []
+    result_training_janta = []
+    result_test_almoco = []
+    result_test_janta = []
+    nota_training_almoco = []
+    nota_training_janta = []
+    nota_test_almoco = []
+    nota_test_janta = []
+    for j in range(len(dados_treino_almoço.index)):
+        result = []
+        for i in range(10):
+            if i == int(np.floor(dados_treino_almoço["{}".format(restaurante)].iloc[j]/500)):
+                result.append(1)
+            else:
+                result.append(0)
+        result_training_almoco.append([result])
+        nota_training_almoco.append(int(almoco_categorias_ordenadas.index.get_loc(dados_treino_almoço['Categoria do Prato'].iloc[j])))
+    for j in range(len(dados_treino_janta.index)):
+        result = []
+        for i in range(10):
+            if i == int(np.floor(dados_treino_janta["{}".format(restaurante)].iloc[j]/500)):
+                result.append(1)
+            else:
+                result.append(0)
+        result_training_janta.append([result])
+        nota_training_janta.append(int(janta_categorias_ordenadas.index.get_loc(dados_treino_janta['Categoria do Prato'].iloc[j])))
+    
+    for j in range(len(dados_test_almoço.index)):
+        result = []
+        for i in range(10):
+            if i == int(np.floor(dados_test_almoço["{}".format(restaurante)].iloc[j]/500)):
+                result.append(1)
+            else:
+                result.append(0)
+        result_test_almoco.append([result])
+        nota_test_almoco.append(int(almoco_categorias_ordenadas.index.get_loc(dados_test_almoço['Categoria do Prato'].iloc[j])))
+    for j in range(len(dados_test_janta.index)):
+        result = []
+        for i in range(10):
+            if i == int(np.floor(dados_test_janta["{}".format(restaurante)].iloc[j]/500)):
+                result.append(1)
+            else:
+                result.append(0)
+        result_test_janta.append([result])
+        nota_test_janta.append(int(janta_categorias_ordenadas.index.get_loc(dados_test_janta['Categoria do Prato'].iloc[j])))
+
+    # Dados de treino e teste pro almoço
+    for i in range(len(dados_treino_almoço.index)):
+        training_data_almoco.append([np.array([dados_treino_almoço.index[i].day, dados_treino_almoço.index[i].month, int(dados_treino_almoço["Precipitação"].iloc[i])/1000, nota_training_almoco[i]]), result_training_almoco[i]])
+    for i in range(len(dados_test_almoço.index)):
+        test_data_almoco.append([np.array([dados_test_almoço.index[i].day, dados_test_almoço.index[i].month, int(dados_test_almoço["Precipitação"].iloc[i])/1000, nota_test_almoco[i]]), result_test_almoco[i]])
+
+    #Dados de treino e teste pra janta
+    for i in range(len(dados_treino_janta.index)):
+        training_data_janta.append([np.array([dados_treino_janta.index[i].day, dados_treino_janta.index[i].month, int(dados_treino_janta["Precipitação"].iloc[i])/1000, nota_training_janta[i]]), result_training_janta[i]])
+    for i in range(len(dados_test_janta.index)):
+        test_data_janta.append([np.array([dados_test_janta.index[i].day, dados_test_janta.index[i].month, int(dados_test_janta["Precipitação"].iloc[i])/1000, nota_test_janta[i]]), result_test_janta[i]])
+
+
+    return (training_data_almoco, test_data_almoco, training_data_janta, test_data_janta)
+    #return (main_almoco_sem_nan, main_janta_sem_nan)
---
+++ src/network.py
@@ -0,0 +1,142 @@
+"""
+network.py
+~~~~~~~~~~
+
+Módulo de implementação de rede neural por modelo de gradiente estocástico em mini-batch.
+
+"""
+
+#### Bibliotecas
+import random
+import numpy as np
+
+class Network(object):
+
+    def __init__(self, sizes):
+        """O parâmetro ``sizes`` deve ser uma lista contendo o número
+        de neurônios em cada camada. Por exemplo, a lista [3, 10, 1] representa
+        uma rede neural com 3 entradas, 10 neurônios na primeira camada oculta e 
+        1 neurônio de saída. Os pesos (weights) e viéses (biases) são inicializados
+        aleatoriamente em distribuição normal."""
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x)
+                        for x, y in zip(sizes[:-1], sizes[1:])]
+
+    def feedforward(self, a):
+        """Função que retorna a saída de uma entrada ``a`` na rede neural."""
+        for b, w in zip(self.biases, self.weights):
+            a = sigmoid(np.dot(w, a)+b)
+        return a
+
+    def SGD(self, training_data, epochs, mini_batch_size, eta,
+            test_data=None):
+        """Função responsável por treinar os pesos e viéses por gradiente estocástico
+        em mini-batch. Perceba que após cada época, é mostrado como a rede neural
+        esta performando em um conjunto de dados teste (test_data)."""
+        if test_data: n_test = len(test_data)
+        n = len(training_data)
+        for j in range(epochs):
+            random.shuffle(training_data)
+            mini_batches = [
+                training_data[k:k+mini_batch_size]
+                for k in range(0, n, mini_batch_size)]
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, eta)
+            if test_data:
+                print("Epoch {}: {} / {} = {}%".format(j, self.evaluate(test_data), n_test, int(self.evaluate(test_data))*100/int(n_test)))
+                #print("Epoch {}: {} / {}, Acertos: {}".format(j, self.evaluate(test_data), n_test, self.acertos(test_data)))
+            else:
+                print("Epoch {} complete".format(j))
+
+    def update_mini_batch(self, mini_batch, eta):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        self.weights = [w-(eta/len(mini_batch))*nw
+                        for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b-(eta/len(mini_batch))*nb
+                       for b, nb in zip(self.biases, nabla_b)]
+
+    def backprop(self, x, y):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        # feedforward
+        activation = [x]
+        activations = [np.array([x]).transpose()] # lista que acumula as activations em cada camada
+        zs = [] # lista que acumula as ativações sem a função de ativação
+        for b, w in zip(self.biases, self.weights):
+            activation = np.array(activation).transpose()
+            z = np.dot(w, activation)+b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+            activation = np.array(activation).transpose()
+        # deltas da última camada
+        delta = self.cost_derivative(np.array(activations[-1]).transpose(), y) * np.array(sigmoid_prime(zs[-1])).transpose()
+        nabla_b[-1] = np.array(delta).transpose()
+        nabla_w[-1] = np.dot(np.array(delta).transpose(), np.array(activations[-2]).transpose())
+        # deltas das camadas anteriores
+        for l in range(2, self.num_layers):
+            z = zs[-l]
+            sp = sigmoid_prime(z)
+            delta = np.dot(np.array(self.weights[-l+1]).transpose(), np.array(delta).transpose()) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, np.array(activations[-l-1]).transpose())
+            delta = np.array(delta).transpose()
+        return (nabla_b, nabla_w)
+
+    def evaluate(self, test_data):
+        test_results = [(np.argmax(self.feedforward(np.array([x]).transpose())), np.argmax(y))
+                        for (x, y) in test_data]
+        return sum(x == y for (x, y) in test_results)
+    
+    def acertos(self, test_data):
+        test_results = [(x, np.argmax(self.feedforward(np.array([x]).transpose())), np.argmax(y))
+                        for (x, y) in test_data]
+        acertos = []
+        for (x,x_f,y) in test_results:
+            if x_f == y:
+                acertos.append(x)
+        return acertos
+
+    def cost_derivative(self, output_activations, y):
+        return (output_activations-y)
+
+#### Funções de Ativação
+def sigmoid(z):
+    sigmoid_z = [1.0/(1.0+np.exp(-x)) for x in z]
+    return sigmoid_z
+
+def sigmoid_prime(z):
+    sigmoid_prime_z = []
+    for x in z:
+        sigmoid_prime_z.append(1.0/(1.0+np.exp(-x))*(1 - 1.0/(1.0+np.exp(-x))))
+    return sigmoid_prime_z
+
+"""Implementação"""
+
+import data_loader2_0
+training_data_almoco_RU, test_data_almoco_RU, training_data_janta_RU, test_data_janta_RU = data_loader2_0.load_data("RU")
+training_data_almoco_RS, test_data_almoco_RS, training_data_janta_RS, test_data_janta_RS = data_loader2_0.load_data("RS")
+training_data_almoco_RA, test_data_almoco_RA, training_data_janta_RA, test_data_janta_RA = data_loader2_0.load_data("RA")
+
+net = Network([4,20,20,20,10])
+print("Almoço RU")
+net.SGD(training_data_almoco_RU, 6, 50, 4.0, test_data=test_data_almoco_RU)
+print("Janta RU")
+net.SGD(training_data_janta_RU, 6, 50, 4.0, test_data=test_data_janta_RU)
+
+print("Almoço RS")
+net.SGD(training_data_almoco_RS, 6, 50, 5.0, test_data=test_data_almoco_RS)
+print("Janta RS")
+net.SGD(training_data_janta_RS, 6, 50, 5.0, test_data=test_data_janta_RS)
+
+print("Almoço RA")
+net.SGD(training_data_almoco_RA, 6, 50, 5.0, test_data=test_data_almoco_RA)
+print("Janta RA")
+net.SGD(training_data_janta_RA, 6, 50, 5.0, test_data=test_data_janta_RA)
