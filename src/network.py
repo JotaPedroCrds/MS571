f63de3c640b89b5f68bd25770d507090e329d6c3
JotaPedroCrds <141438696+JotaPedroCrds@users.noreply.github.com>
2025-10-27 19:25:11 -0300

    Update network.py

--- src/network.py
+++ src/network.py
@@ -23,6 +23,10 @@ class Network(object):
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x)
                         for x, y in zip(sizes[:-1], sizes[1:])]
+        self.mean_b = [np.zeros(b.shape) for b in self.biases]
+        self.mean_w = [np.zeros(w.shape) for w in self.weights]
+        self.variance_b = [np.zeros(b.shape) for b in self.biases]
+        self.variance_w = [np.zeros(w.shape) for w in self.weights]
 
     def feedforward(self, a):
         """Função que retorna a saída de uma entrada ``a`` na rede neural."""
@@ -43,24 +47,57 @@ class Network(object):
                 training_data[k:k+mini_batch_size]
                 for k in range(0, n, mini_batch_size)]
             for mini_batch in mini_batches:
-                self.update_mini_batch(mini_batch, eta)
+                state = 1
+                self.update_mini_batch(mini_batch, eta, state)
             if test_data:
                 print("Epoch {}: {} / {} = {}%".format(j, self.evaluate(test_data), n_test, int(self.evaluate(test_data))*100/int(n_test)))
                 #print("Epoch {}: {} / {}, Acertos: {}".format(j, self.evaluate(test_data), n_test, self.acertos(test_data)))
             else:
                 print("Epoch {} complete".format(j))
 
-    def update_mini_batch(self, mini_batch, eta):
+    def update_mini_batch(self, mini_batch, eta, state):
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
         for x, y in mini_batch:
             delta_nabla_b, delta_nabla_w = self.backprop(x, y)
             nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
             nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-        self.weights = [w-(eta/len(mini_batch))*nw
-                        for w, nw in zip(self.weights, nabla_w)]
-        self.biases = [b-(eta/len(mini_batch))*nb
-                       for b, nb in zip(self.biases, nabla_b)]
+        theta_b, theta_w = self.adam(nabla_b, nabla_w, state, eta)
+        self.weights = [w - nw
+                        for w, nw in zip(self.weights, theta_w)]
+        self.biases = [b - nb
+                       for b, nb in zip(self.biases, theta_b)]
+        
+    def adam(self, grad_b, grad_w, state, eta):
+        beta_1 = 0.9
+        beta_2 = 0.999
+        eps = 10**(-8)
+        newg_b = []
+        newg_w = []
+        i=0
+        j=0
+        for (g_b, m_b, v_b) in zip(grad_b, self.mean_b, self.variance_b):
+            m_b = beta_1 * m_b + (1 - beta_1)*g_b
+            m_b_hat = m_b / (1 - beta_1**(state))
+            v_b = beta_2 * v_b + (1-beta_2)*(np.square(g_b))
+            v_b_hat = v_b / (1 - beta_2**(state))
+            quasenewg_b = eta*m_b_hat/(np.sqrt(v_b_hat)+eps)
+            newg_b.append(quasenewg_b)
+            self.mean_b[i] = m_b
+            self.variance_b[i] = v_b
+            i += 1
+        for (g_w, m_w, v_w) in zip(grad_w, self.mean_w, self.variance_w):
+            m_w = beta_1 * m_w + (1 - beta_1)*g_w
+            m_w_hat = m_w / (1 - beta_1**(state))
+            v_w = beta_2 * v_w + (1-beta_2)*(np.square(g_w))
+            v_w_hat = v_w / (1 - beta_2**(state))
+            quasenewg_w = eta*m_w_hat/(np.sqrt(v_w_hat)+eps)
+            newg_w.append(quasenewg_w)
+            self.mean_w[j] = m_w
+            self.variance_w[j] = v_w
+            j += 1
+        state += 1
+        return (newg_b, newg_w)
 
     def backprop(self, x, y):
         nabla_b = [np.zeros(b.shape) for b in self.biases]
@@ -124,20 +161,32 @@ import data_loader
 training_data_almoco_RU, test_data_almoco_RU, training_data_janta_RU, test_data_janta_RU = data_loader.load_data("RU")
 training_data_almoco_RS, test_data_almoco_RS, training_data_janta_RS, test_data_janta_RS = data_loader.load_data("RS")
 training_data_almoco_RA, test_data_almoco_RA, training_data_janta_RA, test_data_janta_RA = data_loader.load_data("RA")
+training_data_RU, test_data_RU = data_loader.load_data_todos("RU")
 
-net = Network([4,20,20,20,10])
+net_RU = Network([4,20,20,10])
+net_RU_a = Network([4,20,20,10])
+net_RU_j = Network([4,20,20,10])
+net_RS_a = Network([4,20,20,10])
+net_RS_j = Network([4,20,20,10])
+net_RA_a = Network([4,20,20,10])
+net_RA_j = Network([4,20,20,10])
+
+#print("RU todos")
+#net_RU.SGD(training_data_RU, 20, 20, 0.75, test_data=test_data_RU)
 print("Almoço RU")
-net.SGD(training_data_almoco_RU, 6, 50, 4.0, test_data=test_data_almoco_RU)
+net_RU_a.SGD(training_data_almoco_RU, 50, 15, 0.001, test_data=test_data_almoco_RU)
 print("Janta RU")
-net.SGD(training_data_janta_RU, 6, 50, 4.0, test_data=test_data_janta_RU)
+net_RU_j.SGD(training_data_janta_RU, 50, 15, 0.001, test_data=test_data_janta_RU)
 
 print("Almoço RS")
-net.SGD(training_data_almoco_RS, 6, 50, 5.0, test_data=test_data_almoco_RS)
+net_RS_a.SGD(training_data_almoco_RS, 50, 15, 0.001, test_data=test_data_almoco_RS)
+
 print("Janta RS")
-net.SGD(training_data_janta_RS, 6, 50, 5.0, test_data=test_data_janta_RS)
+net_RS_j.SGD(training_data_janta_RS, 50, 15, 0.001, test_data=test_data_janta_RS)
 
 print("Almoço RA")
-net.SGD(training_data_almoco_RA, 6, 50, 5.0, test_data=test_data_almoco_RA)
+net_RA_a.SGD(training_data_almoco_RA, 50, 15, 0.001, test_data=test_data_almoco_RA)
+
 print("Janta RA")
-net.SGD(training_data_janta_RA, 6, 50, 5.0, test_data=test_data_janta_RA)
+net_RA_j.SGD(training_data_janta_RA, 50, 15, 0.001, test_data=test_data_janta_RA)
 
